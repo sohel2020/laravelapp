c865d4fea1041ff9edec497e176780982e8ac6e7
Tarikur Rahaman <sohel2020@localhost.localdomain>
2016-08-25 22:54:14 +0600

    First commit

---
+++ app/Http/Controllers/DbCheckController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use DB; 
+
+class DbCheckController extends Controller
+{
+    
+     public function index()
+    {
+    	die('sanny');
+    	/*$database_host = '';
+    	$database_user = '';
+    	$database_password = '';
+    	$database_name = '';
+		try{
+
+		$connection = mysqli_connect($database_host,$database_user,$database_password,$database_name);
+		    	if($connection)
+				   {
+				     echo "Connected successfully to database ";
+				   }else{
+
+				   	 echo "Connection Error";
+				   }
+		        
+		    }
+
+		catch(Exception $e){
+			echo $e->getMessage();
+		}*/
+
+  }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 80px;
+            }
+            .para {
+                font-style: italic;
+                font-size: 55px;
+                color: #666666
+            }
+            p {
+                font-size: 23px;
+                color: #ededed ;
+
+                
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Hello world</div>
+                <button type="button" id="dbconnect" class="btn btn-danger"><p>Database Connection Check</p></button>
+                <div id="connection_message"></div>
+            </div>
+        </div>
+    </body>
+</html>
+
+
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $("#dbconnect").click(function() {
+            $.ajax({
+                type: "POST",
+                url: 'DbCheckController',
+                data: "",
+                success: function(data) {
+                    console.log(data);
+                }
+            })
+        });
+     
+    });
+
+   
+
+</script>
